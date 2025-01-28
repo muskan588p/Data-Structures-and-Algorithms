60a9a7a64c9dcc7f31d0c8633bb213a9441c0197
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-01-28 21:28:51 +0530

    Time: 0 ms (100%), Space: 23.8 MB (50.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,14 +2,21 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(nums[i]==0 && nums[j]!=0){
-                swap(nums[i], nums[j]);
+        int j=-1;
+        for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                j=i;
+                break;
             }
-            i++;
-            j--;
+        }
+        if(j==-1){
+            return;
+        }
+        for(int i=j+1; i<n;i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;                  // increment is importent
+            }
         }
     }
 };
