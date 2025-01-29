56f2e218e85ca7d144e8cf415e547ea859e59eb7
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-01-30 00:53:11 +0530

    Time: 0 ms (100%), Space: 10.3 MB (86.26%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void getallsubsets(vector<int>& arr, vector<int>& ans, int i, vector<vector<int>> &allsubsets){
+        int n=arr.size();
+        if(i==arr.size()){
+            allsubsets.push_back(ans);
+            return;                   //base case return is imporant
+        }
+        ans.push_back(arr[i]);
+        getallsubsets(arr,ans,i+1, allsubsets);
+
+        ans.pop_back();
+
+        int idx=i+1;
+        while(idx<n && arr[idx]==arr[idx-1]){
+            idx++;
+        }
+        getallsubsets(arr,ans,idx, allsubsets);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> allsubsets;
+        vector<int> ans;
+
+        getallsubsets(nums,ans,0,allsubsets);
+        return allsubsets;
+    }
+};
