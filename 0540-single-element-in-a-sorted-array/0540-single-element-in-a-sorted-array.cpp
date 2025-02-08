9c5224fa90aa94da88458bde77f4390bd809733b
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-02-09 00:22:00 +0530

    Time: 0 ms (100%), Space: 26 MB (91.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        // If the array contains only one element, return it
-        if (n == 1) {
-            return nums[0];
+    int singleNonDuplicate(vector<int>& a) {
+        int n=a.size();
+        if(n==1){
+            return a[0];
         }
-
-        // Check the boundary cases
-        if (nums[0] != nums[1]) {
-            return nums[0];
+        if(a[0] != a[1]){
+            return a[0];
         }
-        if (nums[n - 1] != nums[n - 2]) {
-            return nums[n - 1];
+        if(a[n-1] != a[n-2]){
+            return a[n-1];
         }
-        for(int i=1;i<n-1;i++){     //to keep in bounds
-            if(nums[i-1] != nums[i] && nums[i] != nums[i+1]){
-                return nums[i];
+        int l=0;
+        int h=n-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(a[mid-1] != a[mid] && a[mid] != a[mid+1]){
+                return a[mid];
+            }
+            if(mid % 2==1 && a[mid] == a[mid-1] ||
+               mid % 2==0 && a[mid] == a[mid+1]){   //eliminate left half, ele is in right
+                l=mid+1;
+            }
+            else{                                  //eliminate right half, ele is in left
+                h=mid-1;
             }
         }
         return -1;
