66b0a50c2f3fcc688e2216ea296e9dadbdd58f39
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-04-09 12:07:14 +0530

    Time: 0 ms (100%), Space: 59.3 MB (80.41%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -1,20 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        // If the root is null, we create and return a new node
-        if (root == nullptr) {
+        if(root==nullptr){
             return new TreeNode(val);
         }
+        TreeNode* curr=root;
+        while(true){
+            if(val >= curr->val){
+                if(curr->right != nullptr){
+                    curr=curr->right;
+                }
+                else{
+                    curr->right = new TreeNode(val);
+                    break;
+                }
+            }
 
-        // If the value is less than root's value, insert into the left subtree
-        if (val < root->val) {
-            root->left = insertIntoBST(root->left, val);
+            else{  //val <= curr->val
+                if(curr->left != nullptr){
+                    curr=curr->left;
+                }
+                else{
+                    curr->left= new TreeNode(val);
+                    break;
+                }
+            }
         }
-        // If the value is greater than root's value, insert into the right subtree
-        else {
-            root->right = insertIntoBST(root->right, val);
-        }
-
         return root;
     }
 };
