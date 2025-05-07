4b621d9325ab1644b9e2d5f805e6b2a7dab85ab0
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-05-08 01:22:59 +0530

    Time: 639 ms (5.1%), Space: 296.6 MB (5.65%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int n, int curr, int prev, vector<vector<int>> &dp){
+        //base case
+        if(curr==n){
+            return 0;
+        }
+        if(dp[curr][prev+1] != -1){
+            return dp[curr][prev+1];
+        }
+
+        int take=0;                             //include
+        if(prev==-1 || nums[curr] > nums[prev]){
+            take= 1 + solve(nums, n, curr+1, curr, dp);
+        }
+        int notake=0 + solve(nums, n, curr+1, prev, dp);   // exclude
+        dp[curr][prev+1]=max(take, notake);
+
+        return dp[curr][prev+1];
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(nums, n, 0, -1, dp);
+    }
+};
