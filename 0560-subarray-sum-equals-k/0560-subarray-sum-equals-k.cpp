788bdd5a450e9a30bf251cc95561142fcf4c6a6b
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-05-19 19:16:31 +0530

    Time: 2520 ms (8.9%), Space: 34.2 MB (93.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int n=arr.size();
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
         int count=0;
-        vector<int> prefixsum(n,0);
-        unordered_map<int,int> m;   //pfsum, freq
-        
-        prefixsum[0]=arr[0];   //first ele is same
-        for(int i=1;i<n;i++){      //start from i=1 as 0 ele is same
-            prefixsum[i]=prefixsum[i-1]+arr[i];
-        }
-
-        for(int j=0;j<n;j++){
-            if(prefixsum[j] == k){
-                count++;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k){
+                    count++;
+                }
             }
-            int val=prefixsum[j] - k;   //ps[i-1]=ps[j]-k
-            if(m.find(val) != m.end()){   //if val exists
-                count += m[val];       //add the freq for the val ps[i-1]
-            }
-
-            if(m.find(prefixsum[j]) == m.end()){   //if not exists store in map with freq
-                m[prefixsum[j]] = 0;
-            }
-            m[prefixsum[j]]++;
         }
         return count;
     }
