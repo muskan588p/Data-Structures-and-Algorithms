04b95feb8b812fb031f88e28fc760b4a61a10fa6
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-05-25 23:07:31 +0530

    Time: 611 ms (5.94%), Space: 296.6 MB (11.74%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -21,7 +21,7 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        vector<vector<int>> dp(n, vector<int>(n+1, -1));
         return solve(nums, n, 0, -1, dp);
     }
 };
