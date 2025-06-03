85e7e3a4c86983d8c175e9b37b6458bf89fc2945
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-06-03 22:36:13 +0530

    Time: 22 ms (88.97%), Space: 16.5 MB (62.69%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& vis) {
+    int m = grid.size(), n = grid[0].size();
+    
+    // Boundary or already visited or water check
+    if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0' || vis[i][j])
+        return;
+    
+    vis[i][j] = true;  // mark current cell as visited
+
+    // Explore 4 directions
+    dfs(i + 1, j, grid, vis);
+    dfs(i - 1, j, grid, vis);
+    dfs(i, j + 1, grid, vis);
+    dfs(i, j - 1, grid, vis);
+}
+
+int numIslands(vector<vector<char>>& grid) {
+    int m = grid.size();
+    int n = grid[0].size();
+    // if (m == 0 || n == 0 || grid == nullptr)
+    if (m == 0 || n == 0 )
+        return 0;
+
+    vector<vector<bool>> vis(m, vector<bool>(n, false));
+    int count = 0;
+
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (grid[i][j] == '1' && !vis[i][j]) {
+                ++count;
+                dfs(i, j, grid, vis);
+            }
+        }
+    }
+
+    return count;
+}
+
+
+};
