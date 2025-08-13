73c6a6c00a1eea5cf3b993efa45581f3a52b9eb2
Muskan <124757289+muskan588p@users.noreply.github.com>
2025-08-13 12:10:24 +0530

    Time: 10 ms (48.53%), Space: 18.3 MB (23.95%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int solve(vector<int>& prices, int idx, int buy, vector<vector<vector<int>>> &dp, int limit){
+        if(idx==prices.size()){
+            return 0;
+        }
+        if(limit==0){
+            return 0;
+        }
+        if(dp[idx][buy][limit] != -1){
+            return dp[idx][buy][limit];
+        }
+        int profit=0;
+        if(buy){
+            int take= -prices[idx] + solve(prices, idx+1, 0, dp, limit);
+            int skip= 0 + solve(prices, idx+1, 1, dp, limit);
+            profit=max(take, skip);
+        }
+        else{
+            int take= prices[idx] + solve(prices, idx+1, 1, dp, limit-1); //trans is complete only when sell
+            int skip= 0 + solve(prices, idx+1, 0, dp, limit);
+            profit=max(take, skip);
+        }
+        dp[idx][buy][limit]=profit;
+        return dp[idx][buy][limit];
+    }
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        int limit=k;
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k+1,-1)));
+        return solve(prices, 0, 1, dp, limit);  //first one will take buy
+    }
+};
